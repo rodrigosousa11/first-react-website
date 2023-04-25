80ea2a49e1545ff611fa151b7aab244251cc179a
roh <rodrigosousa1105@gmail.com>
2023-04-25 21:07:17 +0100

    Terminal text now passed as a prop to Terminal component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import React, { useState, useEffect } from "react";
 import Terminal from "./components/Terminal.js";
 import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
 
 function App() {
+    const [showTerminal, setShowTerminal] = useState(false);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setShowTerminal(true);
+        }, 300);
+
+        return () => clearTimeout(timeout);
+    }, []);
+
+    const lines = React.useMemo(
+        () => [
+            "Hi there! My name is Rodrigo Sousa and I'm from Porto, Portugal.",
+            "I'm currently on my second year of a Software Engeneering degree at Universidade Lusófona.",
+            "I love learning new things, especially about coding.",
+            "\u00A0",
+            "Feel free to contact me and thanks for stopping by :)",
+            "\u00A0",
+            "$ ",
+        ],
+        []
+    );
+
     return (
         <div className="App">
             <Header />
-            <Terminal />
+            {showTerminal && <Terminal lines={lines} />}
             <Footer />
         </div>
     );
--- src/components/Terminal.js
+++ src/components/Terminal.js
@@ -1,52 +1,41 @@
 import React, { useState, useEffect } from "react";
 import "../styles/terminal.css";
 
-const Terminal = () => {
-  const lines = React.useMemo(() => [
-    "Hi there! My name is Rodrigo Sousa and I'm from Porto, Portugal.",
-    "I'm a Computer Engeneering student at Universidade Lusófona.",
-    "I love learning new things, especially about new ways of coding.",
-    "In my free time, you can find me working on some project I'm focused on or watching something about cars.",
-    "\u00A0",
-    "Feel free to contact me and thanks for stopping by :)",
-    "\u00A0",
-    "$ ",
-  ], []);
+const Terminal = ({ lines }) => {
+    const [currentLineIndex, setCurrentLineIndex] = useState(0);
+    const [currentLine, setCurrentLine] = useState("");
+    const [writing, setWriting] = useState(false);
 
-  const [currentLineIndex, setCurrentLineIndex] = useState(0);
-  const [currentLine, setCurrentLine] = useState("");
-  const [writing, setWriting] = useState(false);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentLine((prev) => prev + lines[currentLineIndex][prev.length]);
+            setWriting(true);
+        }, 15);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentLine((prev) => prev + lines[currentLineIndex][prev.length]);
-      setWriting(true);
-    }, 15);
+        if (currentLine.length === lines[currentLineIndex].length) {
+            clearInterval(interval);
 
-    if (currentLine.length === lines[currentLineIndex].length) {
-      clearInterval(interval);
+            if (currentLineIndex < lines.length - 1) {
+                setCurrentLineIndex((prev) => prev + 1);
+                setCurrentLine("");
+            }
+            setWriting(false);
+        }
 
-      if (currentLineIndex < lines.length - 1) {
-        setCurrentLineIndex((prev) => prev + 1);
-        setCurrentLine("");
-      }
-      setWriting(false);
-    }
+        return () => clearInterval(interval);
+    }, [currentLine, currentLineIndex, lines]);
 
-    return () => clearInterval(interval);
-  }, [currentLine, currentLineIndex, lines]);
-
-  return (
-    <div id="terminal">
-      {lines.slice(0, currentLineIndex).map((line, i) => (
-        <p key={i}>{line}</p>
-      ))}
-      <p>
-        {currentLine}
-        <span id="cursor" className={writing ? "not-blinking" : ""}></span>
-      </p>
-    </div>
-  );
+    return (
+        <div id="terminal">
+            {lines.slice(0, currentLineIndex).map((line, i) => (
+                <p key={i}>{line}</p>
+            ))}
+            <p>
+                {currentLine}
+                <span id="cursor" className={writing ? "not-blinking" : ""}></span>
+            </p>
+        </div>
+    );
 };
 
 export default Terminal;
